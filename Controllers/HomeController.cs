583794883aa74e09ad18bca196e446ca5324ebfe
TxnUkeGrl <txnukegrl@gmail.com>
2021-07-25 16:33:09 -0400

    first commit, static images

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Lab27Reddit.Data;
+using Lab27Reddit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27Reddit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RedditDAL _redditPost = new RedditDAL();
+
+        public async Task<IActionResult> Index()
+        {
+            var redditPost = await _redditPost.GetPostAsync();
+            return View(redditPost);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/RedditDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27Reddit.Data
+{
+    public class RedditDAL
+    {
+        public HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.reddit.com"); ///r/aww/.json
+            return client;
+        }
+
+        public async Task<RedditModel> GetPostAsync()
+        {
+            var client = GetHttpClient();
+            var response = await client.GetAsync("/r/aww/.json");
+            //install-package Microsoft.AspNet.WebAPI.Client
+            var redditPost = await response.Content.ReadAsAsync<RedditModel>();
+            return redditPost;
+        }
+    }
+}
